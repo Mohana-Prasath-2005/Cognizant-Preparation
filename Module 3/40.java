5111fddb854e3f729988026946cc6e1302b388f5
MOHANA PRASATH G <153210313+Mohana-Prasath-2005@users.noreply.github.com>
2025-05-27 18:16:24 +0530

    Delete Module 3 directory

--- Module 3/40.java
+++
@@ -1,7 +0,0 @@
-public class VirtualThreads {
-    public static void main(String[] args) throws Exception {
-        for (int i = 0; i < 100_000; i++) {
-            Thread.startVirtualThread(() -> System.out.println("Hello from virtual thread!"));
-        }
-    }
-}
